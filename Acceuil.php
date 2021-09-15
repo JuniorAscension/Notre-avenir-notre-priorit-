e7ecfc7d01c81be8647824017f9bae4b847b4b4a
Junior Ascension <atchoujunior9gmail.com>
2021-09-15 13:32:43 +0100

    started acceuil

--- Acceuil.php
+++ Acceuil.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="Style.css">
     <title>Document</title>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#"><img class="logo" src="Logo.png" alt="Logo du site"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -17,33 +18,54 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Acceuil <span class="sr-only"></span></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Nos formations
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="#">Pâtisserie</a>
+          <a class="dropdown-item" href="#">Cosmétique</a>
+          <a class="dropdown-item" href="#">Agro-alimentaire</a>
         </div>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">A propos</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Conctatez-nous</a>
       </li>
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
   </div>
+  <nav class="navbar navbar-light bg-light">
+  <form id="search" class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button id="bouton" class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
 </nav>
-
+</nav>
+<div class="container-fluid">
+  <section class="hero-section">
+      <h1>Bienvenue sur notre site.</h1>
+      <h2>Notre Avenir Notre Priorité</h2>
+  </section>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-6 para1">
+      <p class="titre"><h3><u>Que devrons-nous savoir ?</u></h3></p>
+      <p>Beaucoup de besoins sont communs à tous les hommes: manger, s’abriter et s’habiller, par exemple.On peut facilement s’imaginer des millions de femmesdans toutes les parties du monde en train de peler des
+      patates pour nourrir leur famille; des ouvriers en trainde construire des maisons et des métiers tissant des millions de verges de tissus pour nos vêtements.Mais il existe aussi des milliers de besoins personnels dont la poursuite distrait notre ennui. <br>
+      C'est notamment là qu'intervient le Projet Notre Avenir Notre Priorité afin de faire des jeunes leaders d'aujourd'hui, de véritables hommes capables d'auto entreprendre.Ce projet offre la possibilité de recevoir des formations en <em>Pâtisserie</em>,
+      en <em>Cosmétique</em> et en <em>Agro-Alimentaire</em>.Découvrer ces formations et lancer vous à entreprendre afin d'assurer votre avenir.
+      </p>
+    </div>
+    <div class="col-lg-6">
+      <img src="Argent.jpeg" alt="image">
+    </div>
+  </div>
+</div>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
