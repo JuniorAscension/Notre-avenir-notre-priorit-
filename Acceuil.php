26c0eb35253dcbf523f09b77403f628dbd12feac
Junior Ascension <atchoujunior9gmail.com>
2021-09-15 22:35:16 +0100

    Acceuil at 90%

--- Acceuil.php
+++ Acceuil.php
@@ -66,31 +66,59 @@
     </div>
   </div>
 </div>
-<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+<div class="container">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+   <ol class="carousel-indicators">
+     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="Pâtisserie.png" id="Pâtisserie" class="d-block w-100" alt="un">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="Cosmétique.png" id="Cosmétique" class="d-block w-100" alt="deux">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="Agroalimentaire.png" id="Agroalimentaire" class="d-block w-100" alt="trois">
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="sr-only"></span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
+    <span class="sr-only"></span>
+  </a>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-4">
+      <h3><a href="#">Pâtisserie</a></h3>
+      <img src="Gâteau.jpg" id="Gâteau" alt="photo">
+      <p class="Gâteau">
+      Les mets relevant de la pâtisserie sont sucrés et la plupart du temps nécessitent une cuisson.Comme pour la cuisine salée, il existe de grandes bases. Il existe cinq grands types de pâtes, et les divers gâteaux se rattachent la plupart du temps à l'un d'eux. Pour que la pâtisserie soit réussie, il faut respecter les principes liés à la préparation de ces différentes pâtes. 
+      Dans cette rubrique apprenez à faire des <b>gâteaux cake</b>, des <b>gâteaux madeleine</b> et des <b>biscuits</b>.
+      </p>
+    </div>
+    <div class="col-4">
+      <h3><a href="#">Cosmétique</a></h3>
+      <img src="Teint.jpg" id="Teint" alt="photo">
+      <p class="Teint">
+      Les cosmétiques sont des produits d'hygiène et d'embellissement qui restent superficiels dans leurs actions. On a vu se développer récemment des produits cosmétiques « Bio » ou « Naturel ». Les caractéristiques permettant l'utilisation de ces qualificatifs ont fait l'objet de longues discussions. L'appellation « Bio » est réservée à des produits cosmétiques dont les ingrédients sont issus de l'agriculture biologique. Elle fait l'objet de certifications délivrées par diverses organisations privées (ECOCERT en France, SOIL Association, BDIH, AB, etc. ailleurs), utilisant chacune leur référentiel d'évaluation du produit et de ses ingrédients.
+      Découvrez au cours d'une formation meilleure, à faire du <b>savon de bain</b>, du <b>du savon liquide</b>, du <b>savon en poudre</b>, du <b>parfum</b>, <b>lait et pommade du corp</b>...
+      </p>
+  </div>
+  <div class="col-4">
+    <h3><a href="#">Agroalimentaire</a></h3>
+    <img src="Huile.jpg" id="Huile" alt="photo">
+    <p class="Huile">
+    Se dit des produits agricoles conditionnés ou transformés par l'industrie. C'est un secteur d'activité allant de la production agricole aux denrées alimentaires élaborées à partir de ressources naturelles et des techniques industrielles. Ce domaine englobe les phases de transformation, conservation et de commercialisation des produits se situant entre le stade agricole et celui de la consommation. N'hésitez pas à vous lancer dans un vaste domaine en apprenant à préparer de la <b>mayonnaise</b>, du <b>vinaigre</b>, des <b>sirops</b>, des <b>jus de tout genre de fruit</b> et pleines d'autres préparations à découvrir dans la rubrique Agroalimentaire de nos formation.
+    </p>
+  </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
